0789b9c98e20389a4da413af9c3480191f0b7bbb
varit05 <varitpatel@gmail.com>
2014-02-20 23:07:35 +0530

    Chronic Technologies
    Version 2014.2.1.1 This Version Includes Horizontal Scrolling for
    Hardware,Software and About Us ! Basic Framework is mostly created.

--- js/global.js
+++ js/global.js
@@ -57,16 +57,16 @@ $(window).scroll(function(){
    }                   
 });
 
-
-/*$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		if (target.length) {
-		$('html,body').animate({
-		scrollTop: target.offset().top
-		}, 1000);
-		return false;
-		}
-		}
-		});	*/
+('ul.nav a').bind('click',function(event){
+        var $anchor = $(this);
+        
+      //  if you want to use one of the easing effects:
+        /*$('html, body').stop().animate({
+            scrollLeft: $($anchor.attr('href')).offset().left
+        }, 500,'easeInOutExpo');*/
+         
+        $('html, body').stop().animate({
+            scrollLeft: $($anchor.attr('href')).offset().left
+        }, 1000);
+        event.preventDefault();
+    });
